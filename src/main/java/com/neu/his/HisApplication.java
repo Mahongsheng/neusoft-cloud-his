7366ef3987d34606034a680057539ba71bd7328a
mahongsheng <863253972@qq.com>
2019-07-22 17:07:58 +0800

    即将进行界面处理

--- src/main/java/com/neu/his/HisApplication.java
+++ src/main/java/com/neu/his/HisApplication.java
@@ -1,9 +1,11 @@
 package com.neu.his;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.neu.his.dao")
 public class HisApplication {
 
     public static void main(String[] args) {
