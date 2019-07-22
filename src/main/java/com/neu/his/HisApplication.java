a30181c442b1fa620c1ac63736d22c125681b70e
mahongsheng <863253972@qq.com>
2019-07-22 10:18:09 +0800

    创建项目

---
+++ src/main/java/com/neu/his/HisApplication.java
@@ -0,0 +1,13 @@
+package com.neu.his;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neu/his/HisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neu.his;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
